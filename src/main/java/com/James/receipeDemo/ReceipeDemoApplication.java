aeaae1c1edb9bd6b2655274586cfe36995917a88
james781004 <james781004@gmail.com>
2018-06-16 13:48:13 +0800

    first commit

---
+++ src/main/java/com/James/receipeDemo/ReceipeDemoApplication.java
@@ -0,0 +1,12 @@
+package com.James.receipeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceipeDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/James/receipeDemo/ReceipeDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.James.receipeDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReceipeDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
