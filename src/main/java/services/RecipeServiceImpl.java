31b597de8f1f3b200a16d6c8962b320975b03f22
james781004 <james781004@gmail.com>
2018-06-19 20:56:06 +0800

    add delete function

--- src/main/java/services/RecipeServiceImpl.java
+++ src/main/java/services/RecipeServiceImpl.java
@@ -66,4 +66,9 @@ public class RecipeServiceImpl implements RecipeService {
 		return recipeToRecipeCommand.convert(findById(l));
 	}
 
+	@Override
+	public void deleteById(Long idToDelete) {
+		recipeRepository.deleteById(idToDelete);
+	}
+
 }
